21e9bf4984488ddaf81ca5ffd63878fc09aa7d55
Margolin96 <margolin96@gmail.com>
2023-02-21 20:44:56 +0100

    Demo environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 
 import { TruncatedText } from './TruncatedText';
 
 function App() {
+  const [offset, setOffset] = useState(0);
+
   const items = useMemo(() => {
     return new Array(2000).fill(null).map((_, index) => {
       return {
         text: `feature/create-new-text-ellipsis-component-TC2018.${index}`,
-        tail: index % 20,
+        tail: index % 50 + offset,
       };
     })
-  }, []);
+  }, [offset]);
 
   return (
     <div className="App">
+      <input
+        type="number"
+        value={offset}
+        onChange={(e) => {
+          setOffset(Number(e.target.value));
+        }}
+      />
+
       <table>
         <tbody>
           { items.map(({ text, tail }, index) =>
             <tr key={index}>
+              <td width={200}>
+                <TruncatedText
+                  children={text}
+                  tailLength={tail}
+                  title={text}
+                />
+              </td>
+
               <td width={200}>
                 <TruncatedText
                   children={text}
