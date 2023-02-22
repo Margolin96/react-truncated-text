76b040d3ec3c21979f6d170d7445d76db4dfae99
Margolin96 <margolin96@gmail.com>
2023-02-22 15:43:27 +0100

    Find in page supports, Copy without line break

--- src/TruncatedText.tsx
+++ src/TruncatedText.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useLayoutEffect, useMemo, useRef } from 'react';
 
 import './TruncatedText.css';
 
@@ -15,6 +15,7 @@ export function TruncatedText({
   title = '',
   className = '',
 }: ITruncatedTextProps) {
+  const ref = useRef(null);
   const text = useMemo(() => {
     return (tailLength === 0)
       ? [children, '']
@@ -23,9 +24,25 @@ export function TruncatedText({
         children.slice(-tailLength),
       ];
   }, [children, tailLength]);
+  
+  useLayoutEffect(() => {
+    const node = ref.current;
+    console.log(node);
+    const handler = (event: ClipboardEvent) => {
+      event.clipboardData.setData(
+        'text/plain',
+        document.getSelection().toString().replace('\n', ''),
+      );
+      event.preventDefault();
+    };
+
+    node.addEventListener('copy', handler);
+    return () => node.removeEventListener('copy', handler);
+  }, [ref]);
 
   return (
-    <div className={`truncated-text ${className}`} title={title}>
+    <div className={`truncated-text ${className}`} title={title} ref={ref}>
+      <span className='truncated-text__full'>{ children }</span>
       <span className='truncated-text__truncated'>{ text[0] }</span>
       <span className='truncated-text__tail'>{ text[1] }</span>
     </div>
